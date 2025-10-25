59af6d394a45bb5b362e309ec8ee60295f669d77
Ron <edriron@gmail.com>
2025-10-25 23:04:13 +0300

    components skeleton added, shadcn components installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl">Home</h1>
+      <Button variant={"outline"} size="lg" className="capitalize m-8">
+        Click me
+      </Button>
     </div>
   );
 }
